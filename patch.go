a652fa0312d72bd6c9b96171eb087b5745fdeec0
Monmohan Singh <monmohan@gmail.com>
2016-05-17 13:48:07 +0800

    fix golint errors

--- patch.go
+++ patch.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by a BSD-style
 // license that can be found in the LICENSE file.
 
-//Package data provides the basic interfaces around binary diff and patching process
+//Package xferspdy provides the basic interfaces around binary diff and patching process
 package xferspdy
 
 import (
